20a258393405702c12f326533e17ca63ad4d9e16
Ido Pesok <idopesok91@gmail.com>
2024-03-03 00:45:08 -0800

    feat(assistantMessageContent): added to tools few shot

--- packages/ai-actions/src/tools.ts
+++ packages/ai-actions/src/tools.ts
@@ -74,6 +74,7 @@ type TCreateFewShotToolCallMessages<
 > = (
   examples: {
     userMessageContent: string
+    assistantMessageContent?: string
     tool_calls: ValuesOf<{
       [K in TActionRegistrySubset<TRegistry, U>]: {
         name: ReturnType<TRegistry[K]["getFunctionName"]>
@@ -81,17 +82,20 @@ type TCreateFewShotToolCallMessages<
       } & (ReturnType<TRegistry[K]["getActionType"]> extends "ECHO"
         ? {}
         : ReturnType<TRegistry[K]["getOutputSchema"]> extends z.ZodVoid
-        ? {}
-        : { response: z.output<ReturnType<TRegistry[K]["getOutputSchema"]>> })
+          ? {}
+          : { response: z.output<ReturnType<TRegistry[K]["getOutputSchema"]>> })
     }>[]
   }[]
 ) => OpenAI.Chat.Completions.ChatCompletionMessageParam[]
 
-
-type TChooseTool<TRegistry extends TAnyActionRegistry, U extends (keyof TRegistry)[] | undefined = undefined> = (
-  name: ReturnType<TRegistry[TActionRegistrySubset<TRegistry, U>]["getFunctionName"]>
-) => OpenAI.Chat.Completions.ChatCompletionToolChoiceOption;
-
+type TChooseTool<
+  TRegistry extends TAnyActionRegistry,
+  U extends (keyof TRegistry)[] | undefined = undefined,
+> = (
+  name: ReturnType<
+    TRegistry[TActionRegistrySubset<TRegistry, U>]["getFunctionName"]
+  >
+) => OpenAI.Chat.Completions.ChatCompletionToolChoiceOption
 
 export const setupFunctionCalling = <
   TActionData extends TAnyActionData,
@@ -114,7 +118,7 @@ export const setupFunctionCalling = <
 ): {
   tools: OpenAI.Chat.Completions.ChatCompletionTool[]
   toolCallsHandler: TToolCallHandler<T, U>
-  chooseTool: TChooseTool<T, U>;
+  chooseTool: TChooseTool<T, U>
   createFewShotToolCallMessages: TCreateFewShotToolCallMessages<T, U>
 } => {
   const { inArray } = args
@@ -170,9 +174,9 @@ export const setupFunctionCalling = <
         content:
           result.status === "error"
             ? JSON.stringify({
-              status: "error",
-              message: result.message,
-            })
+                status: "error",
+                message: result.message,
+              })
             : JSON.stringify(result.data),
       }
 
@@ -236,6 +240,13 @@ export const setupFunctionCalling = <
                 : JSON.stringify({}),
         })
       }
+
+      if (example.assistantMessageContent) {
+        messages.push({
+          role: "assistant",
+          content: example.assistantMessageContent,
+        })
+      }
     }
 
     return messages
@@ -243,13 +254,15 @@ export const setupFunctionCalling = <
 
   const chooseTool: TChooseTool<T, U> = (name) => {
     if (!actionIds.includes(functionNameToActionIdMap[name])) {
-      throw new Error(`Action name "${name}" is not allowed.`);
+      throw new Error(`Action name "${name}" is not allowed.`)
     }
     return {
-      type: 'function',
-      function: { name: registry[functionNameToActionIdMap[name]].getFunctionName() },
-    };
-  };
+      type: "function",
+      function: {
+        name: registry[functionNameToActionIdMap[name]].getFunctionName(),
+      },
+    }
+  }
 
   return {
     tools,
