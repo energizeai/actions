d955db020d78852dbde3049e7ad552a9c4c9a9b3
Ido Pesok <idopesok91@gmail.com>
2024-03-14 15:30:19 -0700

    fix(tests): remove testing registries

--- apps/ade/registry/provider.tsx
+++ apps/ade/registry/provider.tsx
@@ -3,31 +3,12 @@
 import {
   ActionRegistriesProviderWrapper,
   TActionRegistriesContext,
-  createAction,
-  createActionsRegistry,
-  createClientActionsRegistry,
   createUseActionRegistries,
-  generateActionRegistryFunctions,
 } from "ai-actions"
 import { createContext, useContext } from "react"
 import { ClientActionsRegistry } from "./client"
 
-const generator = generateActionRegistryFunctions({ namespace: "test" })
-const otherRegistry = generator.createtestActionsRegistry([
-  generator.createtestAction({ id: "test" }).input({}).noHandler(),
-])
-const otherClientREgistry = createClientActionsRegistry(otherRegistry, {})
-
-const anotherRegistry = createActionsRegistry([
-  createAction({ id: "test" }).input({}).noHandler(),
-])
-const anotherClientRegistry = createClientActionsRegistry(anotherRegistry, {})
-
-const Registries = [
-  ClientActionsRegistry,
-  otherClientREgistry,
-  anotherClientRegistry,
-]
+const Registries = [ClientActionsRegistry]
 
 export const ActionRegistriesContext =
   createContext<TActionRegistriesContext>(undefined)
